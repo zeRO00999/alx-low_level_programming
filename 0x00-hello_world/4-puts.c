9416a7b2c4c483989c2866a494e8ee5723c122e2
zeRO00999 <zeronine700@gmail.com>
2023-09-07 04:44:01 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 
- int main(){
-	puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+ {
+puts("\"Programming is like building a multilingual puzzle");
 
-		return 0;
+return(0);
 }
