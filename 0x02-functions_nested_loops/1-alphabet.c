0e770f80d1b7c96994ec89b41f304633f4584b22
zeRO00999 <zeronine700@gmail.com>
2023-09-12 08:49:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include  "main.h"
+/**
+ * ain - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	 char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+_putchar(c);
+}
+_putchar('/n');
+}
