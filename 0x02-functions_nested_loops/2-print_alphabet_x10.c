d870316f2687d879e1630ee1e33b6f93764a1bf8
zeRO00999 <zeronine700@gmail.com>
2023-09-12 09:25:07 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include  "main.h"
+/**
+ * print_alphabetx10 - wirte the the lower-case alphabet on line.
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void print_alphabetx10(void)
+{
+	 char c;
+	 int i;
+		for (i = 0; i < 10 ; i++)
+	{
+		for (c = 'a' ; c <= 'z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
